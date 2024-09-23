9a4ec4d05560a92a06a5605b84c637424b7328ec
Shahil0511 <shahilkumar0511@gmail.com>
2024-09-24 03:40:46 +0530

    feat: Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import tseslint from 'typescript-eslint'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest', // You can specify the ECMAScript version
+        sourceType: 'module' // Use ES modules
+      }
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  {
+    plugins: {
+      react: pluginReact
+    },
+    rules: {
+      'react/display-name': 'warn' // Or "error" depending on your preference
+      // Add other rules as needed
+    }
+  }
+]
+
